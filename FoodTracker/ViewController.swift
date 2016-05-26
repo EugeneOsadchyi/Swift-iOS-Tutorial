93aca7f8ef9d9748d204276ac3e0337740af6767
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2016-05-26 16:02:39 +0300

    Initialize repository. Add text field delegation

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Yevhen Osadchyi on 5/25/16.
+//  Copyright © 2016 Yevhen Osadchyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        nameTextField.text = "Default Text"
+    }
+
+}
+
