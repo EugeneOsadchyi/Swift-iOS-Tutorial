c6d4d1e9af512660354bfe05c39dba8b2a02369b
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2016-05-31 16:56:22 +0300

    Add an ability to select the image. Initialize Rating controll.

---
+++ FoodTracker/RaitingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RaitingControl.swift
+//  FoodTracker
+//
+//  Created by Yevhen Osadchyi on 5/31/16.
+//  Copyright © 2016 Yevhen Osadchyi. All rights reserved.
+//
+
+import UIKit
+
+class RaitingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
