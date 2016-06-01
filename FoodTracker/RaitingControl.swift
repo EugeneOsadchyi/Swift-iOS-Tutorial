bb34d41df5e17dfd28cc5b8a295335a477c1a367
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2016-06-01 13:47:25 +0300

    Remove hardcoded star button count and spacing

--- FoodTracker/RaitingControl.swift
+++ FoodTracker/RaitingControl.swift
@@ -15,13 +15,16 @@ class RatingControl: UIView {
     var rating = 0
     var ratingButtons = [UIButton]()
 
+    let spacing = 5
+    let starCount = 5
+
 
     // MARK: Initialization
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
 
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
             button.backgroundColor = UIColor.redColor()
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
@@ -38,7 +41,7 @@ class RatingControl: UIView {
 
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerate() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
             button.frame = buttonFrame
         }
     }
