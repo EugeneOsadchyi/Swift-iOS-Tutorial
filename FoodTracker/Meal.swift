f0d6ba711ea3c854217bcdc0ddbbf564e06dec42
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2016-06-01 15:22:12 +0300

    Add data properties and initializer to Meal model

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2016 Yevhen Osadchyi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    // Mark: Propperties
+
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+
+        // initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
