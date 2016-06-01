a42fd8139a5f6c4ba97b9a9b080291a9b77d8690
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2016-06-01 13:17:00 +0300

    Add rating button. Bind TouchDown event to the button from code.

--- FoodTracker/RaitingControl.swift
+++ FoodTracker/RaitingControl.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
-class RaitingControl: UIView {
+class RatingControl: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    // MARK: Initialization
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+
+        addSubview(button)
+
     }
-    */
 
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+
+
+    // MARK: Button Action
+
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
 }
