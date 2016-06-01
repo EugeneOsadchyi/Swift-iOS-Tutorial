bd21f5149b588a4461668e4f068931a2f5c30118
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2016-06-01 16:20:52 +0300

    Initialize TableViewCell

---
+++ FoodTracker/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  FoodTracker
+//
+//  Created by Yevhen Osadchyi on 6/1/16.
+//  Copyright © 2016 Yevhen Osadchyi. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
